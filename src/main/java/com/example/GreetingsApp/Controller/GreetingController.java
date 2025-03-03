b38135a6a68eea66e89468673a9e1c6e6763cddd
AashiiMittal <aashimittal2004@gmail.com>
2025-03-03 11:36:38 +0530

    Greeting app with Service layer

--- src/main/java/com/example/GreetingsApp/Controller/GreetingController.java
+++ src/main/java/com/example/GreetingsApp/Controller/GreetingController.java
@@ -1,11 +1,24 @@
 package com.example.GreetingsApp.Controller;
 import org.springframework.web.bind.annotation.*;
+import com.example.GreetingsApp.Service.GreetingService;
+import org.springframework.beans.factory.annotation.Autowired;
 @RestController
 @RequestMapping("/greeting")
 public class GreetingController {
-    @GetMapping
+    @GetMapping("/simple")
     public String getGreeting() {
         return "Hello, this is a GET request!";
+    }
+
+    private final GreetingService greetingService;
+    @Autowired
+    public GreetingController(GreetingService greetingService) {
+        this.greetingService = greetingService;
+    }
+
+    @GetMapping("/service")
+    public String getGreetingServiceLayer() {
+        return GreetingService.getGreetingMessage();
     }
 }
 
